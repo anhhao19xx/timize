22a545b5d73ff46c89921149172893681d13d1e0
Anh Hao <hi@anhhao.me>
2021-12-19 22:44:22 +0700

    Replace markdown editor by quill

--- web/comps/TimizeEditor.vue
+++ web/comps/TimizeEditor.vue
@@ -1,106 +1,74 @@
 <template>
-  <div class="tm-editor">
-    <b-form-textarea
-      class="content-editor"
-      ref="textarea"
-      max-rows="9999"
-      :value="content"
-      @input="onInput"
-    ></b-form-textarea>
-
-    <MdViewer :value="previewData" class="content-preview" @change="onInput" @route="$emit('route', $event)"></MdViewer>
-  </div>
+  <quill-editor
+    class="tm-editor"
+    ref="timizeEditor"
+    v-model="content"
+    :options="editorOption"
+    @blur="onEditorBlur($event)"
+    @focus="onEditorFocus($event)"
+    @ready="onEditorReady($event)"
+  />
 </template>
 
 <script>
-import MdViewer from './MdViewer.vue';
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 
 export default {
-  props: ['value'],
-  components: { MdViewer },
+  components: {
+    quillEditor
+  },
 
-  data(){
+  data () {
     return {
-      previewData: '',
-      content: '',
-      polling: null,
-      isChanged: true
+      content: '<h2>I am Example</h2>',
+      editorOption: {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], 
+            [{ header: 1 }, { header: 2 }],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+          ]
+        }
+      }
     }
   },
 
   methods: {
-    pollData () {
-      this.polling = setInterval(() => {
-        if (!this.isChanged)
-          return;
+    onEditorBlur(quill) {
+      
+    },
 
-        this.previewData = this.content;
-        this.isChanged = false;
-      }, 500)
+    onEditorFocus(quill) {
+      
     },
     
-    onInput(value){
-      this.content = value;
-      this.isChanged = true;
-      this.$emit('input', this.content);
+    onEditorReady(quill) {
+      
+    },
+
+    onEditorChange({ quill, html, text }) {
+      
+    }
+  },
+
+  computed: {
+    editor() {
+      return this.$refs.timizeEditor.quill
     }
   },
 
   mounted() {
-    this.$nextTick(() => {
-      this.$refs.textarea.focus();
-    });
-
-    this.pollData();
-
-    this.content = this.value;
-  },
-
-  beforeDestroy () {
-    clearInterval(this.polling)
-  },
-
-  watch: {
-    value(){
-      if (this.value === this.content)
-        return;
-
-      this.content = this.value;
-      this.isChanged = true;
-    }
+   
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .tm-editor {
-  display: flex;
-  height: 100%;
-
-  .content-editor, 
-  .content-preview {
-    width: 50%;
-    color: black;
-    padding: 0.375rem 0.75rem;
-  }
-
-  .content-editor {
-    overflow-y: scroll;  
-    font-family: inherit;
-    border: none;
-    border-radius: 0;
-    height: 100% !important;
-  }
-
-  .content-editor:focus {
-    box-shadow: none;
-    outline: none;
-    border: none;
-  }
-
-  .content-preview {
-    border-left: 1px solid #e0e0e0;
-    overflow-y: scroll;
-  }
+  
 }
 </style>
